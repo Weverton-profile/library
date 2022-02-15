df5419f03b89f383139fdcf6ed001b7e91903de3
Weverton-profile <wevertonalves.profile@gmail.com>
2022-02-15 00:05:56 -0300

    configuring routes and more

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'about/index'
+  get 'welcome/index'
   resources :books
+
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
